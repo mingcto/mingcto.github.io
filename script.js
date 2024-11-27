1382f09be7a576e004689dc70617e46073cd3127
monyun2023 <monyun2023@proton.me>
2024-11-28 00:27:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,10 @@ window.onload = () => {
         });
     });
 
+    // 延时隐藏海图并显示主内容
+    setTimeout(() => {
+        document.getElementById('sea-map').style.opacity = 0;
+        document.getElementById('main-content').classList.remove('hidden');
+        document.getElementById('main-content').style.opacity = 1;
+    }, 3000);
+};
