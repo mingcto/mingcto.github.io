c117ddb8a6ed8601fae625a1e981f54e22f3fea8
monyun2023 <monyun2023@proton.me>
2024-11-28 07:22:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ window.onload = () => {
     // 为按钮添加点击事件，直接访问链接
     document.getElementById('tg-btn').addEventListener('click', () => {
         console.log("TG按钮被点击");
-        window.location.href = 'https://www.baidu.com'; // 替换为 Telegram 链接
+        window.location.href = 'https://t.co/zyYsMnlXPM'; // 替换为 Telegram 链接
     });
 
     document.getElementById('twitter-btn').addEventListener('click', () => {
         console.log("推特按钮被点击");
-        window.location.href = 'https://www.baidu.com'; // 替换为 Twitter 链接
+        window.location.href = 'https://x.com/MINGToken'; // 替换为 Twitter 链接
     });
 
     document.getElementById('dex-btn').addEventListener('click', () => {
         console.log("DEX按钮被点击");
-        window.location.href = 'https://www.baidu.com'; // 替换为 DEX 链接
+        window.location.href = 'https://pump.fun/coin/4JvuLhSDs2soJUEd6bzpWsC43zCUqJBj65aQ3vu6pump'; // 替换为 DEX 链接
     });
 };
